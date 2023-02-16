04bb62023f2d1ae7758bb421de6b7710f0a2c0a9
tuandev1l <tuanprogrammer001@gmail.com>
2023-02-16 15:12:59 +0700

    last commit

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -12,8 +12,8 @@ import chatBarSlice from '../../store/chatBarSlice';
 
 interface Props {}
 
-// export const socket = io('https://chat-app-mern-jd3z.onrender.com/');
-export const socket = io('http://localhost:3000');
+export const socket = io('https://chat-app-mern-jd3z.onrender.com/');
+// export const socket = io('http://localhost:3000');
 
 const Chat = ({}: Props) => {
   const conSelector = useSelector(conversationSelector);
